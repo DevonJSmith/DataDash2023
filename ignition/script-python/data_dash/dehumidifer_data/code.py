d94d291344ddb8bdc0e63fa015b2b1e811b844e7
DevonJSmith <djamessmith03@gmail.com>
2023-09-10 17:47:37 -0400

    implement tuya get requests add timed scripts to refresh data

--- ignition/script-python/data_dash/dehumidifer_data/code.py
+++ ignition/script-python/data_dash/dehumidifer_data/code.py
@@ -1,4 +1,32 @@
-Dehumidifer_Tag_Base = '[default]djamessmith03/Basement Dehumidifer'
+Dehumidifier_Tag_Base = '[default]djamessmith03/Basement Dehumidifer'
+
+def RefreshBasicDeviceDetails():
+	'''
+		Calls the Tuya Cloud API to retrieve basic info (name, update timestamp, etc) for the dehumidifer
+	'''
+	deviceId = data_dash.SECRETS.DEVICE_ID
+	tuyaAPI = data_dash.tuya_api.TuyaApi()
+	
+	tagPaths = []
+	tagValues = []
+	
+	details = tuyaAPI.RetrieveDeviceDetails(deviceId)
+	# date fields
+	# create time
+	tagPaths.append('%s/Create Time' % Dehumidifier_Tag_Base)
+	tagValues.append(system.date.fromMillis(details['create_time'] * 1000))
+	# active time
+	tagPaths.append('%s/Active Time' % Dehumidifier_Tag_Base)
+	tagValues.append(system.date.fromMillis(details['active_time'] * 1000))
+	# update time
+	tagPaths.append('%s/Update Time' % Dehumidifier_Tag_Base)
+	tagValues.append(system.date.fromMillis(details['update_time'] * 1000))
+	# device name
+	tagPaths.append('%s/Device Name' % Dehumidifier_Tag_Base)
+	tagValues.append(details['name'])
+	
+	system.tag.writeAsync(tagPaths = tagPaths, values = tagValues)
+#END RefreshBasicDeviceDetails
 
 def RefreshAnalyticData():
 	'''
@@ -27,7 +55,7 @@ def RefreshAnalyticData():
 		elif propName == 'filterClean':
 			tagName = 'Filter Clean'
 		#END if
-		tagPaths.append('%(baseTagPath)s/Analytics/%(tagName)s' % {'baseTagPath': Dehumidifer_Tag_Base, 'tagName': tagName})
+		tagPaths.append('%(baseTagPath)s/Analytics/%(tagName)s' % {'baseTagPath': Dehumidifier_Tag_Base, 'tagName': tagName})
 		tagValues.append(prop['value'])
 	#END for
 	system.tag.writeAsync(tagPaths = tagPaths, values = tagValues)
--- ignition/script-python/data_dash/tuya_api/code.py
+++ ignition/script-python/data_dash/tuya_api/code.py
@@ -1,63 +1,165 @@
+import hmac
+import hashlib
 class TuyaApi(object):
-	def RetrieveDeviceProperties(self,deviceId):
+	BASE_URL = 'https://openapi.tuyaus.com'
+	
+	def __init__(self):
+		self._access_code = None
+		self._access_code_expiration = None
+	#END __init__
+	
+	@property
+	def access_code(self):
+		if self._access_code is None or self._access_code_expiration is None or system.date.now() >= self._access_code_expiration:
+			self._authenticate()
+		#END if
+		return self._access_code
+	#END access-code
+
+	def _authenticate(self):
+		'''
+			Gets an Auth Token from the Tuya API
+		'''
+		timeStamp = system.date.toMillis(system.date.now())
+		queryUrl = '/v1.0/token?grant_type=1'
+		headers = {
+			'client_id': data_dash.SECRETS.CLIENT_ID,
+			't': timeStamp,
+			'sign_method': 'HMAC-SHA256',
+			'nonce': None,
+			'stringToSign': None,
+			'sign': None
+		}
+		signMap = self._generateSignMap(method = 'get', url = queryUrl)
+		headers['stringToSign'] = signMap['signUrl'].replace('\n','')
+		headers['sign'] = self._generateSignCode(
+			clientId = data_dash.SECRETS.CLIENT_ID,
+			timestamp = timeStamp,
+			nonce = None,
+			signStr = signMap['signUrl'],
+			secret = data_dash.SECRETS.CLIENT_SECRET
+		)
+		
+		resp = system.net.httpGet(
+			url = self.BASE_URL + queryUrl,
+			headerValues = headers
+		)
+		jsonResp = system.util.jsonDecode(resp)
+		self._access_code = jsonResp['result']['access_token']
+		token_lifetime_seconds = jsonResp['result']['expire_time']
+		self._access_code_expiration = system.date.addSeconds(system.date.now(), token_lifetime_seconds - 120)
+	#END _authenticate
+	
+	def _generateSignMap(self, method, url):
+		'''
+			This algorithm has been taken from the examples available in the Tuya API
+			https://developer.tuya.com/en/docs/iot/new-singnature?id=Kbw0q34cs2e5g
+		'''
+		sha256 = ''
+		headersStr = ''
+		signMap = {}
+		signArr = []
+		
+		hashObj = hashlib.sha256()
+		hashObj.update(sha256)
+		sha256 = hashObj.hexdigest()
+		
+		signMap = {
+			'url': url,
+			'signUrl': method.upper() + '\n' + sha256 + '\n' + headersStr + '\n' + url 
+		}
+		return signMap
+	#END _generateSignMap
+
+	def _generateSignCode(self, clientId, timestamp, nonce, signStr, secret, access_code = None):
+		'''
+			generates a sign code for authenticating with Tuya API
+			Reference: https://developer.tuya.com/en/docs/iot/new-singnature?id=Kbw0q34cs2e5g
+		'''
+		strToHash = None
+		if access_code is not None:
+			strToHash = clientId + str(access_code) + str(timestamp) + str(nonce) + signStr
+		else:
+			strToHash = clientId + str(timestamp) + str(nonce) + signStr
+		#END if
+		hashedStr = hmac.new(
+		    secret,
+		    msg=strToHash,
+		    digestmod=hashlib.sha256
+		).hexdigest().upper()
+		return hashedStr
+	#END _generateSignCode
+	
+	def RetrieveDeviceProperties(self, deviceId):
 		'''
 			Pull current values of all available properties on device
 			API reference: https://developer.tuya.com/en/docs/cloud/116cc8bf6f?id=Kcp2kwfrpe719
-			@PARAM deviceId: string - the deviceId to retrieve data for
+			@PARAM deviceId: string - the id of the device to retrieve data for
 			@RETURN: list<dict> - the json results from the API
 		'''
-		# TODO: implement fully
-		response = {
-		  'result': {
-		    'properties': [
-		      {
-		        'code': 'switch_on',
-		        'custom_name': '',
-		        'dp_id': 1,
-		        'time': 1690929978364,
-		        'value': True
-		      },
-		      {
-		        'code': 'set_temper',
-		        'custom_name': '',
-		        'dp_id': 6,
-		        'time': 1690929984387,
-		        'value': 15
-		      },
-		      {
-		        'code': 'wind_speed',
-		        'custom_name': '',
-		        'dp_id': 8,
-		        'time': 1690930858112,
-		        'value': '2'
-		      },
-		      {
-		        'code': 'fault',
-		        'custom_name': '',
-		        'dp_id': 15,
-		        'time': 1690929238118,
-		        'value': 0
-		      },
-		      {
-		        'code': 'f_temper',
-		        'custom_name': '',
-		        'dp_id': 18,
-		        'time': 1694309036820,
-		        'value': 66
-		      },
-		      {
-		        'code': 'filterClean',
-		        'custom_name': '',
-		        'dp_id': 25,
-		        'time': 1690929165248,
-		        'value': True
-		      }
-		    ]
-		  },
-		  'success': True,
-		  't': 1694309042141,
-		  'tid': 'b972af854f7811ee8711ea6e14ba735f'
+		timeStamp = system.date.toMillis(system.date.now())
+		queryUrl = '/v2.0/cloud/thing/%s/shadow/properties' % deviceId
+		headers = {
+			'client_id': data_dash.SECRETS.CLIENT_ID,
+			'access_token': self.access_code,
+			't': timeStamp,
+			'sign_method': 'HMAC-SHA256',
+			'nonce': None,
+			'stringToSign': None,
+			'sign': None
 		}
+		signMap = self._generateSignMap(method = 'get', url = queryUrl)
+		headers['stringToSign'] = signMap['signUrl'].replace('\n','')
+		headers['sign'] = self._generateSignCode(
+			clientId = data_dash.SECRETS.CLIENT_ID,
+			timestamp = timeStamp,
+			nonce = None,
+			signStr = signMap['signUrl'],
+			secret = data_dash.SECRETS.CLIENT_SECRET,
+			access_code = self.access_code
+		)
+		
+		resp = system.net.httpGet(
+			url = self.BASE_URL + queryUrl,
+			headerValues = headers
+		)
+		response = system.util.jsonDecode(resp)
 		return response['result']['properties']
 	#END RetrieveDeviceProperties
+	
+	def RetrieveDeviceDetails(self, deviceId):
+		'''
+			Retrieves base details about device from Tuya API
+			API Reference: https://developer.tuya.com/en/docs/cloud/3829469013?id=Kcp2l2v9wma0m
+			@PARAM deviceId: string - the id of the device to retrieve info for
+		'''
+		timeStamp = system.date.toMillis(system.date.now())
+		queryUrl = '/v1.0/devices/%s' % deviceId
+		headers = {
+			'client_id': data_dash.SECRETS.CLIENT_ID,
+			'access_token': self.access_code,
+			't': timeStamp,
+			'sign_method': 'HMAC-SHA256',
+			'nonce': None,
+			'stringToSign': None,
+			'sign': None
+		}
+		signMap = self._generateSignMap(method = 'get', url = queryUrl)
+		headers['stringToSign'] = signMap['signUrl'].replace('\n','')
+		headers['sign'] = self._generateSignCode(
+			clientId = data_dash.SECRETS.CLIENT_ID,
+			timestamp = timeStamp,
+			nonce = None,
+			signStr = signMap['signUrl'],
+			secret = data_dash.SECRETS.CLIENT_SECRET,
+			access_code = self.access_code
+		)
+		
+		resp = system.net.httpGet(
+			url = self.BASE_URL + queryUrl,
+			headerValues = headers
+		)
+		response = system.util.jsonDecode(resp)
+		return response['result']
+	#END RetrieveDeviceDetails
 #END TuyaApi
